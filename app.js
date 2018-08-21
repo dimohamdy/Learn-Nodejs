05bbd33985cadf8f4aa3a7212d79d0d8744176cc
dimohamdy <dimo.hamdy@gmail.com>
2018-08-21 18:19:51 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const os = require("os");
+// const asd2 = require("./asd2.js")
+// var info = os.userInfo();
+// console.log(`hello my name is ${info.username} ${asd2.age}`);
+// asd2.addNumber()
+//
+// var num = asd2.addNumber2(10,20)
+// console.log(num);
+
+const _ = require("lodash");
+
+let array = _.uniq([1,2,4,5,2,1,3]);
+console.log(array);
---
+++ notes.js
@@ -0,0 +1,13 @@
+console.log("Hello Notes")
+module.exports.age = 123;
+
+
+
+module.exports.addNote = () =>{
+  console.log("Add Note");
+};
+
+
+module.exports.addNumber2 = (num1,num2) =>{
+return num1 + num2;
+};

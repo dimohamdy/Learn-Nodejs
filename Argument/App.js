43e2a6fc031c53c0bd178954da03153da772346e
dimohamdy <dimo.hamdy@gmail.com>
2018-08-21 19:50:30 +0200

    access argument
    access argument from command line

---
+++ Argument/App.js
@@ -0,0 +1,22 @@
+
+
+
+let command = process.argv[2]
+
+switch (command) {
+
+  case "add":
+  console.log("add");
+  break;
+    case "remove":
+    console.log("remove");
+
+  break;
+  case "change":
+  console.log("change");
+  break;
+  default:
+  console.log("not define command");
+
+
+}
